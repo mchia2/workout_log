fb614ed9f83e7d7a192973ad147de276e383400a
Matthias Chia <mchia2@gmail.com>
2015-07-30 01:38:49 -0400

    add exercises to workouts

---
+++ db/migrate/20150730050742_create_exercises.rb
@@ -0,0 +1,13 @@
+class CreateExercises < ActiveRecord::Migration
+  def change
+    create_table :exercises do |t|
+      t.string :name
+      t.integer :sets
+      t.integer :reps
+      t.integer :weight
+      t.references :workout, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
