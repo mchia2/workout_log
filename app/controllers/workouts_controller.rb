5b5050153c5bacee9a5abeb5f74ffbfea564e792
Matthias Chia <mchia2@gmail.com>
2015-07-31 16:20:16 -0400

    Added authentication for index and show to try and fix heroku bug

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,6 @@
 class WorkoutsController < ApplicationController
 	before_action :find_workout, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!, #except: [:index, :show]
+	before_action :authenticate_user!, except: [:index, :show]
 	
 	def index
 		# if
